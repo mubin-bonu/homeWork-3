ee9ac7a4127122e6b6ce11e5d4847d81632675c1
mubin-bonu <mubin.me@mail.ru>
2023-03-25 11:39:31 +0600

    props and state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function ClickCounter(props) {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <button onClick={handleClick}> Click
+        {props.label}: {count}
+      </button>
   );
 }
 
-export default App;
+export default ClickCounter;
